f42018b4b38c4bc5cfd63884ab4b93df147619ed
jovanac99 <comjovana99@gmail.com>
2022-07-09 15:34:27 +0200

    Kreiran React projekat, početna strana sadrži dva dugmeta za registraciju i login, u App.js postavljena ruta za početnu stranu

---
+++ src/pages/HomePage.js
@@ -0,0 +1,19 @@
+function HomePage() {
+    return (
+        <div className="hp-div">
+
+            <div className="backround-image">
+
+                <h1 id="wlcm">Welcome to our shop</h1>
+
+                <button class="btn btn-lg btn-light" id="rg">Register</button>
+                <button class="btn btn-lg btn-light" id="lg">Login</button>
+
+
+            </div>
+
+        </div>
+    )
+}
+
+export default HomePage;
